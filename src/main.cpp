16020492a1c30608fae1f8828775f49c099f192d
WangHanSolo <hwang638@wisc.edu>
2023-08-09 05:25:21 +0900

    temp

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,7 @@ int main(int argc, char* argv[]) {
     // Initialize Google's logging library
     google::InitGoogleLogging(argv[0]);
 
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
     LOG(INFO) << "Hello World";
 }
