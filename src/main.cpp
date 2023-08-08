91d0ae73cd9f97159cf80eb0c65ac9cecc602c3d
Han Wang <43941252+WangHanSolo@users.noreply.github.com>
2023-08-09 05:56:41 +0900

    adding default logging levels (#2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <glog/logging.h>
 
 int main(int argc, char* argv[]) {
-    // Initialize Google's logging library
-    google::InitGoogleLogging(argv[0]);
 
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    // if no glog related flags passed, we assume INFO level logging
+    if (argc < 2){
+        FLAGS_stderrthreshold = 0;
+        google::InitGoogleLogging(argv[0]);
+    } else{
+        google::InitGoogleLogging(argv[0]);
+        gflags::ParseCommandLineFlags(&argc, &argv, true);
+    }
+
 
     LOG(INFO) << "Hello World";
 }
