69150472929d91668435115e60208a33234fd967
Wang Han <han@Wangs-MacBook-Pro.local>
2023-10-24 17:22:41 -0500

    building gflag in root build dir

--- build_libs_unix.sh
+++ build_libs_unix.sh
@@ -5,14 +5,15 @@ echo $ROOT
 
 git submodule update --init --recursive
 
-pushd third_party/gflags
-mkdir build_cmake
-cd build_cmake
-cmake     -DCMAKE_DEBUG_POSTFIX="" -DBUILD_SHARED_LIBS=ON -DINSTALL_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$ROOT/third_party/gflags/build_cmake/local_install ..
+mkdir build_cmake && cd build_cmake
+mkdir third_party && cd third_party
+
+cmake  -DCMAKE_DEBUG_POSTFIX="" -DBUILD_SHARED_LIBS=ON -DINSTALL_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$ROOT/build_cmake/third_party/gflags $ROOT/third_party/gflags
 make -j4
 make install
-popd
-cd $ROOT
+cd $ROOT/build_cmake
 
+cmake ..
+make -j4
 
 # TODO: should I also add glog and gtest here?
