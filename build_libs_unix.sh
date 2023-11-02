379d935203950c6b6972bf4ddd369ace742c4e57
Han Wang <43941252+WangHanSolo@users.noreply.github.com>
2023-11-01 22:08:48 -0500

    mkdir -p .... (#12)

--- build_libs_unix.sh
+++ build_libs_unix.sh
@@ -5,10 +5,9 @@ echo $ROOT
 
 git submodule update --init --recursive
 
-mkdir build_cmake && cd build_cmake
-mkdir third_party && cd third_party
+mkdir -p build_cmake/third_party/gflags
+cd build_cmake/third_party/gflags
 
-mkdir gflags      && cd gflags
 cmake  -DCMAKE_DEBUG_POSTFIX="" -DBUILD_SHARED_LIBS=ON -DINSTALL_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$ROOT/build_cmake/third_party/gflags $ROOT/third_party/gflags
 make -j4
 make install
