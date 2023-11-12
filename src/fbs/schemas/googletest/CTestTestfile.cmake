43b35dd0ad8ecd5e7510727c6ac99874d7652fa1
WangHanSolo <hwang638@wisc.edu>
2023-11-12 13:51:46 -0500

    remove build artifact

--- src/fbs/schemas/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/han/git/shojin/third_party/googletest
-# Build directory: /Users/han/git/shojin/core/fbs/schemas/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- src/fbs/schemas/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/han/git/shojin/third_party/googletest/googletest
-# Build directory: /Users/han/git/shojin/core/fbs/schemas/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
